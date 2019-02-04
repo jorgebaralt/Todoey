e3510bd73d19a450a19c5ab0217ef99a99e23421
jorgebaralt <jorgebaraltq@gmaik.com>
2019-02-04 18:12:22 -0500

    Encode and Decode data

--- Todoeym/Controllers/TodoListViewController.swift
+++ Todoeym/Controllers/TodoListViewController.swift
@@ -12,26 +12,11 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newItem = Item()
-        newItem.title = "FindMike"
-        itemArray.append(newItem);
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2);
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3);
-        
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-//            itemArray = items
-//        }
-        // Do any additional setup after loading the view, typically from a nib.
+        loadItems()
     }
 
     //MARK - Table view datasource methods
@@ -53,11 +38,12 @@ class TodoListViewController: UITableViewController {
     }
     
     //MARK - Table viee Delegate Methods
-    //fire when we click on any cell in the table view
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         // togle checkmark
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //update local storage
+        saveItems()
         
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
@@ -65,25 +51,61 @@ class TodoListViewController: UITableViewController {
     
     //MARK - Add new items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
         // alert with text to add a new item
         let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
+        
         // add a textfield to the alert
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
+        
         //add an action (button) to alert
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //append item to array
+            
+            //create item
             let newItem = Item()
             newItem.title = textField.text!
+            
+            //Append item to array
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray");
+            self.saveItems()
+           
+            
             self.tableView.reloadData()
         }
         alert.addAction(action);
         present(alert,animated: true,completion: nil)
     }
+    
+    //MARK - save and load data
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("Error encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems(){
+        
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("Error decoding item array, \(error)")
+            }
+            
+        }
+
+       
+    }
 }
+
+
 
--- Todoeym/Data Model/Item.swift
+++ Todoeym/Data Model/Item.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Item {
+class Item: Codable {
     var title: String = ""
     // default false
     var done: Bool = false
